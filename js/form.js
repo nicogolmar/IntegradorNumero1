b851b35e459c72b03170415ca4283d76e22c4152
nicogolmar <nico.bocakapo09@gmail.com>
2022-08-12 22:38:12 -0300

    update-index

--- js/form.js
+++ js/form.js
@@ -10,11 +10,20 @@ lastName=document.getElementById('lastName').value;
 email=document.getElementById('email').value;
 msg=document.getElementById('msg').value;
 
+if(nameUser!="" && lastName !="" && email!="" && msg!="" && email.includes('@')) {
 
-console.log(`Name: ${nameUser}`);
-console.log(`Last Name: ${lastName}`);
-console.log(`Email: ${email}`);
-console.log(`Mensaje: ${msg}`);
+
+    console.log(`Name: ${nameUser}`);
+    console.log(`Last Name: ${lastName}`);
+    console.log(`Email: ${email}`);
+    console.log(`Mensaje: ${msg}`);
+    
+    
+}
+
+else{
+    console.log("Invalid Inform")
+}
 
 
 
